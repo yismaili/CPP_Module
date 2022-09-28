04d83d150f4c36f4dc0304054124a98f5c564926
Younes Ismaili <yismaili@e2r11p1.1337.ma>
2022-09-28 22:12:25 +0100

    finish ex00 in  module 02

---
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 21:49:21 by yismaili          #+#    #+#             */
+/*   Updated: 2022/09/28 22:11:34 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string.h>
+class Fixed
+{
+private:
+    int value;
+   static const int bits = 8;
+public:
+    Fixed();
+    ~Fixed();
+   void	operator=(Fixed const  &other);
+   	Fixed(const Fixed  &other);
+   int getRawBits( void ) const;
+   void setRawBits( int const raw );
+    Fixed(const int nbrInt);
+    Fixed(const float nbrFloat);
+    float toFloat( void ) const;
+    int toInt( void ) const;
+};
+std::ostream &operator<<(std::ostream &outputStream, Fixed const &fixedPoint);
