401c76c5f88248adddd3a3122e94ceffbe05848d
Younes Ismaili <yismaili@e2r5p1.1337.ma>
2022-10-21 21:35:04 +0100

    start cpp module 06 finich ex01

---
+++ CPP_Module_06/ex01/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili <yismaili@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 21:22:30 by yismaili          #+#    #+#             */
+/*   Updated: 2022/10/21 21:34:36 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+typedef struct s_data {
+	std::string	msg;
+} Data;
+
+uintptr_t serialize(Data* ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data*>(raw);
+}
+
+int	main(void) {
+	Data*	data = new Data;
+	Data*	tmp;
+
+	data->msg = "yismaili";
+	tmp = deserialize(serialize(data));
+
+	if (data->msg != tmp->msg) {
+        std::cout << " was not successful!!!!" << std::endl;   
+    }
+	else {
+		std::cout << "successful!!!!" << std::endl;
+    }
+	delete data;
+	return (0);
+}
