74de151e8f1d7e2454b7181e4f0c58f15e37641c
Younes Ismaili <yismaili@e2r7p2.1337.ma>
2022-10-31 22:28:16 +0100

    start ex00 in  module 08 cpp

---
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 22:03:28 by yismaili          #+#    #+#             */
+/*   Updated: 2022/10/31 22:20:21 by yismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFINND_HPP
+
+#include <iostream>
+template <typename T>
+T easyfind(T var, int i){
+    
+}
+#endif
