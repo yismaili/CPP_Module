09b98c56964d56d2bf85a2640da59e3eb4ccd551
Younes Ismaili <yismaili@e2r6p1.1337.ma>
2022-11-03 17:57:51 +0100

    finish  module 08 cpp

--- CPP_Module_08/ex02/MutantStack.hpp
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -6,44 +6,36 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 21:56:07 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/02 21:57:44 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/03 17:11:53 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-# include <iostream>
-# include <deque>
-# include <stack>
-# include <list>
+#include <iostream>
+#include <vector>
+#include <stack>
 
-template<typename T, typename C = std::deque<T> >
-class MutantStack : public std::stack<T, C> {
-
-public:
-	MutantStack() {}
-	~MutantStack() {}
-	MutantStack(const MutantStack<T, C> & copy) : std::stack<T, C>::stack(copy) {}
-	MutantStack<T, C> & operator=(const MutantStack<T, C> & assign) {
-		std::stack<T, C>::operator=(assign);
-		return *this;
-	}
-
-	typedef typename std::stack<T, C>::container_type::iterator	iterator;
-	typedef typename std::stack<T, C>::container_type::reverse_iterator	reverse_iterator;
-	typedef typename std::stack<T, C>::container_type::const_iterator	const_iterator;
-	typedef typename std::stack<T, C>::container_type::const_reverse_iterator	const_reverse_iterator;
-
-	iterator				begin() { return this->c.begin(); }
-	reverse_iterator		rbegin() { return this->c.rbegin(); }
-	const_iterator			cbegin() { return this->c.cbegin(); }
-	const_reverse_iterator	crbegin() { return this->c.crbegin(); }
-
-	iterator				end() { return this->c.end(); }
-	reverse_iterator		rend() { return this->c.rend(); }
-	const_iterator			cend() { return this->c.cend(); }
-	const_reverse_iterator	crend() { return this->c.crend(); }
-
+template <typename T, typename container = std::deque<T> >
+class MutantStack : public std::stack<T, container>
+{
+	private:
+	public:
+		MutantStack(/* args */){}	
+		MutantStack(const MutantStack &copy): std::stack<T, container>::stack(copy){}
+		MutantStack<T,container> &operator=(const MutantStack<T,container> &assignCopy)
+		{
+			std::stack<T,container>::operator=(assignCopy);
+			return (*this);
+		}
+		~MutantStack(){}
+		typedef typename std::stack<T, container>::container_type::iterator iterator;
+		iterator begin(){
+			return (this->c.begin());
+		}
+		iterator end(){
+			return (this->c.end());
+		}
 };
 #endif
--- CPP_Module_08/ex02/main.cpp
+++ CPP_Module_08/ex02/main.cpp
@@ -6,11 +6,12 @@
 /*   By: yismaili < yismaili@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 21:55:32 by yismaili          #+#    #+#             */
-/*   Updated: 2022/11/02 21:58:56 by yismaili         ###   ########.fr       */
+/*   Updated: 2022/11/03 17:56:32 by yismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
@@ -31,9 +32,32 @@ int main()
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
     std::stack<int> s(mstack);
+    /*list  ----------------------->*/
+    std::cout<<"list :"<<std::endl;
+    std::list<int> mlist;
+    mlist.push_back(5);
+    mlist.push_back(17);
+    std::cout << mlist.back() << std::endl;
+    mlist.pop_back();
+    std::cout << mlist.size() << std::endl;
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    //[...]
+    mlist.push_back(0);
+    std::list<int>::iterator itList = mlist.begin();
+    std::list<int>::iterator iteList = mlist.end();
+    ++itList;
+    --itList;
+    while (itList != iteList)
+    {
+        std::cout << *itList << std::endl;
+        ++itList;
+    }
+    std::list<int> l(mlist);
     return 0;
 }
